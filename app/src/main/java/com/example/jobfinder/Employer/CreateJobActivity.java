e967edfea83193cee1761f423a21ed177e804d7d
vizielod <elod.vizi@yahoo.com>
2020-05-11 01:00:06 +0200

    Creating and Editing Jobs under Employer users implemented. (Commit before another huge code change, moving/pushing the connection child under the JobIDs)

---
+++ app/src/main/java/com/example/jobfinder/Employer/CreateJobActivity.java
@@ -0,0 +1,14 @@
+package com.example.jobfinder.Employer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_job);
+    }
+}
---
+++ app/src/main/java/com/example/jobfinder/Employer/JobObject.java
@@ -0,0 +1,4 @@
+package com.example.jobfinder.Employer;
+
+public class JobObject {
+}
